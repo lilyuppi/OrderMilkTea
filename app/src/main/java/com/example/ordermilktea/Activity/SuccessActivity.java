3f3a75c7749f0e4417cf067bcf56355eabff3fe6
namnguyen1405 <namnguyen140598@gmail.com>
2019-05-26 15:56:51 +0700

    add SuccessActivity

--- app/src/main/java/com/example/ordermilktea/Activity/SuccessActivity.java
+++ app/src/main/java/com/example/ordermilktea/Activity/SuccessActivity.java
@@ -1,6 +1,7 @@
 package com.example.ordermilktea.Activity;
 
 import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -16,29 +17,51 @@ public class SuccessActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_success);
-        success=findViewById(R.id.successtext);
-        imgsuccess0=findViewById(R.id.blacksuccess);
-        imgsucces1=findViewById(R.id.greensuccess);
+        success = findViewById(R.id.successtext);
+        imgsuccess0 = findViewById(R.id.blacksuccess);
+        imgsucces1 = findViewById(R.id.greensuccess);
 
-        Thread bamgio = new Thread(){ // hàm chạy gif sau 5s tự chuyển sang màn hình chính
-            public void run(){
-                try{
+        Thread bamgio = new Thread() { // hàm chạy gif sau 5s tự chuyển sang màn hình chính
+            public void run() {
+                try {
                     sleep(2000);
                 } catch (InterruptedException e) {
                     e.printStackTrace();
-                }
-                finally {
+                } finally {
                     imgsuccess0.setVisibility(View.VISIBLE);
                     imgsucces1.setVisibility(View.GONE);
                 }
             }
         };
-        imgsuccess0.setVisibility(View.GONE);
-        imgsucces1.setVisibility(View.VISIBLE);
         bamgio.start();
+        final Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                // Do something after 5s = 5000ms
+                imgsuccess0.setVisibility(View.GONE);
+                imgsucces1.setVisibility(View.VISIBLE);
+            }
+        }, 3000);
+
+        Thread chuyen = new Thread() { // hàm chạy gif sau 5s tự chuyển sang màn hình chính
+            public void run() {
+                try {
+                    sleep(3000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                } finally {
+                    Intent intent = new Intent(SuccessActivity.this,MainActivity.class);
+                    startActivity(intent);
+                }
+            }
+        };
+        chuyen.start();
     }
+
     protected void onPause(){
         super.onPause();
         finish();
     }
+
 }
